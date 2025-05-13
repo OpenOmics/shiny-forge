cf97726865e95ce9e625183c0f1de158accb7c0e
Ryan Routsong <routsongrm@nih.gov>
2025-05-13 13:31:19 -0500

    feat: UPDATE, DELETE, LOGS functionality

--- src/parse.py
+++ src/parse.py
@@ -5,10 +5,16 @@ import ipdb
 import os
 from .config import REGION
 from packaging.version import Version, InvalidVersion
+from dockerfile_parse import DockerfileParser
 from typing import Dict, List, Optional, Union
 
 ###
-# functions for executing and parsing structured file
+###  > Objective: encapsulate all parsing related helpers and utilities
+###
+
+
+###
+###  >> Section: functions for executing and parsing structured file formats
 ###
 
 def read_artifacts(file):
@@ -25,24 +31,27 @@ def read_artifacts(file):
     return artifacts
     
 ###
-# functions for executing and parsing gcloud activities
+###  >> Section: functions for executing and parsing GCP artifacts
 ###
 
 def run_and_grab(proc: List, out: Union[List, bool]):
     args = {'stderr': subprocess.PIPE}
+    env = os.environ.copy()
+    env['PYTHONWARNINGS'] = "ignore:Unverified HTTPS request"
+    args['env'] = env
     if out:
         args['stdout'] = subprocess.PIPE
+    
     result = subprocess.run(proc, **args)
     if getattr(result, 'stdout', None):
         return json.loads(result.stdout)
     else:
         return result.stdout
 
-
 def read_all():
     proc = ["gcloud", "run", "services", "list", f"--region={REGION}"]
     outs = run_and_grab(proc, False)
-    return
+    return 
 
 
 def check_job_exists(project_name):
@@ -58,13 +67,45 @@ def read_one(project_name):
         outs = run_and_grab(proc, False)
     else:
         raise ValueError(f'Application "{project_name}" does not exist!')
-    
+
+
+def list_apps():
+    get_apps = run_and_grab(
+                ["gcloud", 
+                 "run", 
+                 "services", 
+                 "list", 
+                 f"--region={REGION}", 
+                 "--format=\"json\""],
+                 True
+               )
+    return [app['metadata']['name'] for app in get_apps]
+
+
+def get_container_for_service(service_name):
+    proc = ["gcloud", "run", "services", "describe", f"--region={REGION}", "--format=\"json\"", service_name]
+    outs = run_and_grab(proc, True)
+    return outs['spec']['template']['spec']['containers'][0]['image']
+
+
+###
+###  >> Section: functions for executing and parsing docker artifacts
+### 
+
+def read_dockerfile(file):
+    file = os.path.abspath(file)
+    if not os.path.exists(file):
+        raise FileNotFoundError('docker file missing')
+    dockerfile = DockerfileParser()
+    dockerfile.content = open(file).read()
+    build_args = [line['value'] for line in dockerfile.structure if line['instruction'] in ('BUILD-ARG', 'ARG')]
+    return build_args
+
 
 def get_docker_tag(image):
     proc = ['gcloud', 'container', 'images', 'list-tags', '--format="json"', image]
     outs = run_and_grab(proc, True)
     tags = []
-
     if len(outs) > 0:
         for tag in outs[0]['tags']:
             try:
@@ -72,14 +113,10 @@ def get_docker_tag(image):
                 tags.append(_t)
             except InvalidVersion:
                 continue
-
     if tags:
         latest_v = sorted(tags)[-1].base_version.split('.')
         latest_minor = int(latest_v[2]) + 1
         latest_tag = f"{latest_v[0]}.{latest_v[1]}.{latest_minor}"
     else:
         latest_tag = '0.0.1'
-
-    
-
     return latest_tag
