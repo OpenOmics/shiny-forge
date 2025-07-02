92d1a81e598aede1e9506ba1aa1de8b35c49f8ad
Ryan Routsong <routsongrm@nih.gov>
2025-07-02 14:34:18 -0400

    dev: print debug

--- docker/shinycell2/base/build_shinycell.R
+++ docker/shinycell2/base/build_shinycell.R
@@ -117,9 +117,8 @@ if (length(missing_args) > 0) {
     quit(status = 1)
 }
 
-fatal(opt$shiny.files)
-
 if (is.null(opt$shiny.files) | is.na(opt$shiny.files) | opt$shiny.files == "" | opt$shiny.files == "NA") {
+    fatal('shouldn\'t be here')
     seurat_obj              <- readRDS(rds_file)
     if (is.null(opt$meta.to.rm) | is.na(opt$max.levels) | opt$meta.to.rm == "" | opt$meta.to.rm == "NA") {
         rm.meta             <- NULL
